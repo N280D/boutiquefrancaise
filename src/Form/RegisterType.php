2a3f903055a6f4db95a3d4bb45ca7827f3573104
N280D <NAOWEL@HOTMAIL.FR>
2022-05-10 15:39:09 +0200

    test enregistrement user bdd

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,9 +23,34 @@ class RegisterType extends AbstractType
 
 
             ])
-            ->add('lastname')
-            ->add('email')
-            ->add('password')
+            ->add('lastname', TextType::class,['label'=>'Nom',
+                    'attr'=>[
+                        'placeholder'=>'Merci de saisir votre nom'
+                    ]
+
+            ])
+            ->add('email', EmailType::class,['label'=>'Email',
+                'attr'=>[
+                    'placeholder'=>'Merci de saisir votre email'
+                ]
+                ])
+            ->add('password',PasswordType::class,
+                ['label'=>'Mot de passe',
+                    'attr'=>[
+                        'placeholder'=>'Merci de saisir un mot de passe'
+                    ]
+                ])
+            ->add('password_confirm',PasswordType::class,
+                ['label'=>'Confirmer votre mot de passe',
+                    'mapped'=>false,
+                    'attr'=>[
+                        'placeholder'=>'Confirmer votre mot de passe'
+                    ]
+                ])
+            ->add('submit',SubmitType::class,
+                ['label'=>"S'inscrire",
+
+            ])
         ;
     }
 
