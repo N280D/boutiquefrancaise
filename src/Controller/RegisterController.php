8ae5dd7dd0cd5f597a8bafbfa54bb6f72be83e27
N280D <NAOWEL@HOTMAIL.FR>
2022-05-12 14:34:21 +0200

    page de connection mise en place et hash mdp

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,39 +8,36 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
     private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->entityManager=$entityManager;
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
     }
 
 
     #[Route('/inscription', name: 'register')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordHasherInterface $hasher): Response
     {
         $user=new User();
-        $form=$this->createForm(RegisterType::class,$user);
+        $form=$this->createForm(RegisterType::class, $user);
 
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
              $user=$form->getData();
+
+              $password=$hasher->hashPassword($user,$user->getPassword());
+             $user->setPassword($password);
              $this->entityManager->persist($user);
              $this->entityManager->flush();
             }
 
-
-
-
         return $this->render('register/index.html.twig',[
                 'form'=>$form->createView()
-
-
         ] );
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -11,42 +11,72 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname', TextType::class, ['label'=>'Prènom',
+            ->add('firstname', TextType::class, [
+                'label'=>'Prènom',
+                'constraints'=>new Length(
+                    [
+                        'min'=>2,
+                        'max'=>30,
+                    ]
+                ),
                 'attr'=>[
                     'placeholder'=>'Merci de saisir votre Prènom'
                 ]
 
 
             ])
-            ->add('lastname', TextType::class,['label'=>'Nom',
-                    'attr'=>[
+            ->add('lastname', TextType::class,[
+                'label'=>'Nom',
+                'constraints'=>new Length( [
+                    'min'=>2,
+                    'max'=>30,
+                ]),
+                'attr'=>[
                         'placeholder'=>'Merci de saisir votre nom'
                     ]
 
             ])
-            ->add('email', EmailType::class,['label'=>'Email',
+            ->add('email', EmailType::class,[
+                'label'=>'Email',
+                'constraints'=>new Length(
+                    [
+                        'min'=>2,
+                        'max'=>30,
+                    ]
+                ),
                 'attr'=>[
                     'placeholder'=>'Merci de saisir votre email'
                 ]
                 ])
-            ->add('password',RepeatedType::class,
-                [
-                    'type='=>PasswordType::class,
-                    'invalid_message'=>'Le mot de passe de confirmation doit être identique au mot de passe',
-                    'label'=>'Mot de passe',
+
+            ->add('password',RepeatedType::class, [
+                    'type'=> PasswordType::class,
+                    'invalid_message'=>'Le mot de passe et la confirmation doivent être identique au mot de passe',
                     'required'=>true,
-                    'first_options'=>['label'=>'Merci de saisir un mot de passe'],
-                    'second_options'=>['label'=>'Merci de saisir un mot de passe à nouveau'],
+                    'first_options'=>[
+                        'label'=>'Merci de saisir un mot de passe',
+                        'attr'=>[
+                            'placeholder'=>'Merci de saisir votre mot de passe'
+                        ]
+
+                    ],
+                    'second_options'=>[
+                        'label'=>'Merci de saisir un mot de passe à nouveau',
+                        'attr'=>[
+                            'placeholder'=>'Merci de confirmer votre mot de passe'
+                        ]
+                    ],
 
                 ])
-            ->add('submit',SubmitType::class,
-                ['label'=>"S'inscrire",
+            ->add('submit',SubmitType::class, [
+                'label'=>"S'inscrire",
 
             ])
         ;
