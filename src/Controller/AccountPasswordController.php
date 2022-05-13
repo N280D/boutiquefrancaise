7926e9b0c91d4635bfe057eb81831f0cd45b66d6
N280D <NAOWEL@HOTMAIL.FR>
2022-05-13 14:55:15 +0200

    ajout du style css

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -3,20 +3,52 @@
 namespace App\Controller;
 
 use App\Form\ChangePassWordType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountPasswordController extends AbstractController
 {
-    #[Route('/compte/changer-mot-de-passe', name: 'account_password')]
-    public function index(): Response
+    private $entityManger;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/compte/changer-mot-de-passe', name: 'account_password')]
+
+    public function index(Request $request, UserPasswordHasherInterface $hasher): Response
+    {
+        $notification=null;
         $user=$this->getUser();
         $form=$this->createForm(ChangePassWordType::class,$user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $old_pwd=$form->get('old_password')->getData();
+
+            if($hasher->isPasswordValid($user,$old_pwd)) {
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $hasher->hashPassword($user, $new_pwd);
+
+                $user->setPassword($password);
+                $this->entityManager->flush();
+                $notification='Votre mot de passe a bien été mis à jour';
+            }
+            else{
+                $notification="Votre mot de passe actuel n'est pas le bon";
+            }
+        }
         return $this->render('account/password.html.twig',
             [
-                'form'=>$form->createView()
+                'form'=>$form->createView(),
+                'notification'=>$notification
             ]);
-    }
+        }
 }
