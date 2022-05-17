909752a7fd9040bb169a139f25bd51542cc60122
N280D <NAOWEL@HOTMAIL.FR>
2022-05-17 14:45:17 +0200

    insertion bdd des differents produits

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -23,7 +24,12 @@ class ProductCrudController extends AbstractCrudController
         return [
 
             TextField::new('name'),
-            ImageField::new('illustration')->setUploadDir('assets/images/'),
+            SlugField::new('slug')->setTargetFieldName('name'),
+            ImageField::new('illustration')
+                ->setBasePath('uploads/')
+                ->setUploadDir('/public/uploads/')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(false),
             TextField::new('subtitle'),
             TextareaField::new('description'),
             MoneyField::new('price')->setCurrency('EUR'),
