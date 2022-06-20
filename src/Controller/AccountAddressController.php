49e74e4dafee5039eb9cd1602ced6e1269b74024
N280D <NAOWEL@HOTMAIL.FR>
2022-06-20 12:49:20 +0200

    modifier addresse de livraison ok

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -45,4 +45,29 @@ class AccountAddressController extends AbstractController
             'form'=>$form->createView()
         ]);
     }
+    #[Route('/compte/modifier-une-adresse/{id}', name: 'account_address_edit')]
+    public function edit (Request $request,$id)
+
+    {
+        $address=$this->entityManager->getRepository(Address::class)->findOneById($id);
+
+        if(!$address||$address->getUser()!=$this->getUser()){
+            return $this->redirectToRoute('account_address');
+
+        }
+        $form=$this->createForm(AddressType::class,$address);
+
+        $form->handleRequest($request);
+
+        if($form ->isSubmitted() && $form->isValid()) {
+
+
+            $this->entityManager->flush();
+            return $this->redirectToRoute('account_address');
+
+        }
+        return $this->render('account/address_add.html.twig',[
+            'form'=>$form->createView()
+        ]);
+    }
 }
