ed35938a2a7ee0cb3db3ca82c3ab3bf6d59cd513
N280D <NAOWEL@HOTMAIL.FR>
2022-06-27 12:06:46 +0200

    gestion de la commande ok

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -5,13 +5,14 @@ namespace App\Controller;
 use App\Classe\Cart;
 use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
     #[Route('/commande', name: 'order')]
-    public function index(Cart $cart): Response
+    public function index(Cart $cart,Request $request): Response
     {
         if (!$this->getUser()->getAddresses()->getValues())
         {
@@ -21,6 +22,12 @@ class OrderController extends AbstractController
         $form=$this->createForm(OrderType::class, null,[
            'user'=>$this->getUser()
         ]);
+        $form->handleRequest($request);
+        if($form ->isSubmitted() && $form->isValid()) {
+
+            dd($form->getData());
+        }
+
         return $this->render('order/index.html.twig',[
             'form'=>$form->createView(),
             'cart'=>$cart->getFull(),
